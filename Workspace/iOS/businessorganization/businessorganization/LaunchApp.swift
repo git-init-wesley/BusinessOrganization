34d5ea1452518a05d9538c9a33cfacd3039ad7e7
Wesley Levasseur <mailpro.levasseur@icloud.com>
2021-09-26 16:35:24 +0200

    Init Xcode project

---
+++ Workspace/iOS/businessorganization/businessorganization/LaunchApp.swift
@@ -0,0 +1,21 @@
+//
+//  Conçu avec ♡ par Levasseur Wesley.
+//  © Copyright 2021. Tous droits réservés.
+//
+//  Création datant du 12/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct LaunchApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ZStack(alignment: .center) {
+                Text("Hello world !")
+            }
+        }
+    }
+
+}
